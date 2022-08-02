c07674f87a77cff9a04a883a1e4284f6947aa47b
Siddharth Rout <67204074+siddharth5096@users.noreply.github.com>
2022-08-02 13:00:40 +0530

    Time: 44 ms (74.88%), Space: 18.6 MB (48.68%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.cpp
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     int helper(string &s, string &t,int i, int j,vector<vector<int>>&dp){
-    if(j<0)
+    if(j==0)
         return 1;
-    if(i<0)
+    if(i==0)
         return 0;
     
     if(dp[i][j]!=-1)
         return dp[i][j];
     
-    if(s[i]==t[j])
+    if(s[i-1]==t[j-1])
         return dp[i][j] = helper(s,t,i-1,j-1,dp) + helper(s,t,i-1,j,dp);
     
     else
